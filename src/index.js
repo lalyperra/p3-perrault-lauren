0f783ee64cc16ae2d0391672aa897a6fe49b1af1
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-04-29 09:31:50 -0700

    try to get generator to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import "./main.css";
-import { footer, header,lunch-generator } from '../modules';
+import { footer, header,lunchGenerator } from '../modules';
 
 // Create a method for this class called APP renderTemplate()
 //Inside of this method, utilize ES6 template strings to build out an index.html structure 
+class App {
+    constructor() {
+        lunchGenerator.addText(){
+
+        }
+    }
+
+    document.body.innerHTML
+
+    app.renderTemplate() 
+
+    const lunchApp = new App();
+    lunchApp.renderTemplate();
--- src/modules/lunch-generator.js
+++ src/modules/lunch-generator.js
@@ -1,64 +1,81 @@
 //array of 10 strings that will show with button click
 //entree.sort(); will go alphabetically
-const foodArray = [
-    {
-        mealTitle: 'Peanut Butter and Jelly',
-        mealImage:  'images/pbj.jpg',
-        mealDescription: 'Jiff PB with Strawberry Jelly on white bread'
-    },
-    {
-        mealTitle: 'Cheeseburger',
-        mealImage:  'images/cheeseburger.jpg',
-        mealDescription: 'Single patty with american cheese'
-    },
-    {
-        mealTitle: 'Chicken Strips',
-        mealImage:  'images/chickenStrips.jpg',
-        mealDescription: '4 chicken strips with ranch sauce'
-    },
-    {
-        mealTitle: 'Pasta Salad',
-        mealImage:  'images/pastaSalad.jpg',
-        mealDescription: 'Cold pasta salad with tomatoes and pepperoni'
-    },
-    {
-        mealTitle: 'Spaghetti',
-        mealImage:  'images/spaghettiMeat.jpg',
-        mealDescription: 'Spaghetti with red sauce and meatballs'
-    },
-     {
-        mealTitle: 'Grilled Cheese',
-        mealImage:  'images/grilledCheese.jpg',
-        mealDescription: 'Grilled white bread with American cheese'
-    },
-    {
-        mealTitle: 'Nachos',
-        mealImage:  'images/nachos.jpg',
-        mealDescription: 'Nacho chips with hamburger meat and salsa'
-    },
-    {
-        mealTitle: 'Ham and Cheese Sliders',
-        mealImage:  'images/hamCheese.jpg',
-        mealDescription: 'Hawaiian roles with ham and swiss cheese'
-    },
-    {
-        mealTitle: 'Rice and Bean Burrito',
-        mealImage:  'images/beanBurrito.jpg',
-        mealDescription: 'Bean burrito with salsa and sour cream'
-    },
-    {
-        mealTitle: 'Chicken Tacos',
-        mealImage:  'images/tacos.jpg',
-        mealDescription: 'Chicken tacos with salsa and sour cream'
-    },
-]
+class lunchGenerator {
+    constructor (){
+        lunchArray = [
+        {
+            mealTitle: 'Peanut Butter and Jelly',
+            mealImage: 'images/pbj.jpg',
+            mealDescription: 'Jiff PB with Strawberry Jelly on white bread'
+        },
+        {
+            mealTitle: 'Cheeseburger',
+            mealImage: 'images/cheeseburger.jpg',
+            mealDescription: 'Single patty with american cheese'
+        },
+        {
+            mealTitle: 'Chicken Strips',
+            mealImage: 'images/chickenStrips.jpg',
+            mealDescription: '4 chicken strips with ranch sauce'
+        },
+        {
+            mealTitle: 'Pasta Salad',
+            mealImage: 'images/pastaSalad.jpg',
+            mealDescription: 'Cold pasta salad with tomatoes and pepperoni'
+        },
+        {
+            mealTitle: 'Spaghetti',
+            mealImage: 'images/spaghettiMeat.jpg',
+            mealDescription: 'Spaghetti with red sauce and meatballs'
+        },
+        {
+            mealTitle: 'Grilled Cheese',
+            mealImage: 'images/grilledCheese.jpg',
+            mealDescription: 'Grilled white bread with American cheese'
+        },
+        {
+            mealTitle: 'Nachos',
+            mealImage: 'images/nachos.jpg',
+            mealDescription: 'Nacho chips with hamburger meat and salsa'
+        },
+        {
+            mealTitle: 'Ham and Cheese Sliders',
+            mealImage: 'images/hamCheese.jpg',
+            mealDescription: 'Hawaiian roles with ham and swiss cheese'
+        },
+        {
+            mealTitle: 'Rice and Bean Burrito',
+            mealImage: 'images/beanBurrito.jpg',
+            mealDescription: 'Bean burrito with salsa and sour cream'
+        },
+        {
+            mealTitle: 'Chicken Tacos',
+            mealImage: 'images/tacos.jpg',
+            mealDescription: 'Chicken tacos with salsa and sour cream'
+        },
+    }
 
-foodArray[0]mealTitle,
-foodArray[0]mealImage,
-foodArray[0]mealDescription,
+    sorting() {
+        for(let i = this.lunchArray.length-1; i > 0; i--){
+            const j = Math.floor(Math.random() * 1)
+            const k = this.lunchArray[i]
+            this.lunchArray[i] = this.lunchArray[j]
+            this.lunchArray[j] = k
+        }
+    }
+
+    addText() {
+        window.onload = ()=> {
+            document.getElementById('button').addEventListener('click', this.sorting);
+            mealTitle.innerHTML = this.lunchArray[0].mealTitle;
+            }
+        }
+}
+
+export const lunchGenerator = new LunchGenerator();
 
 
-//randomize the array, use innerHTML method to add first item each click
+/*randomize the array, use innerHTML method to add first item each click
 function myFunction1() {
     foodArray.sort(function (a, b) { return 0.5 - Math.random() });
     document.getElementById("foodArray").innerHTML = foodArray;
