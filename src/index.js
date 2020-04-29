17481cb08193db4ecb5c950c524679c437ca8243
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-04-28 20:14:31 -0700

    add app.js script to index and add header.js content attempt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./main.css";
+import { footer, header,lunch-generator } from '../modules';
 
 // Create a method for this class called APP renderTemplate()
 //Inside of this method, utilize ES6 template strings to build out an index.html structure 
--- src/modules/header.js
+++ src/modules/header.js
@@ -1,3 +1,20 @@
+class Header {
+
+    loadHeader() {
+        // Creata a new instance
+        const userProfile = new UserProfile();
+
+        // Invoke the method (component)
+        userProfile.loadUserProfile();
+
+        // Output loading status
+        console.log('Header component is loaded...')
+    }
+
+}
+
+export { Header };
+
 //Header with pageTitle and pageDescription
 class Header {
     constructor(pageTitle, pageDescription) {
@@ -8,5 +25,3 @@ class Header {
 
 // Variable with new header
 export const headerText = new Header('What/s in your lunch?');
-
-
